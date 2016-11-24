a08214afa0588aa63c35df71364c9130eb6043dd
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-24 14:13:25 -0500

    Updated triangle reactjs reactor

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 require('babel-polyfill');
 const https = require('https');
 const koa = require('koa');
-const co = require('co');
 const {readFileSync} = require('fs');
 const {join} = require('path');
-const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
+const middleware = require('koa-webpack');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 
-const compile = webpack(webpackConfig);
+const compiler = webpack(webpackConfig);
 
 const app =  new koa();
 
@@ -23,50 +22,25 @@ const options = {
   cert: readFileSync(join(__dirname, 'ca/cert.pem'))
 };
 
-app.use(co.wrap(function* () {
-  return yield users.routes();
-}));
+app.use(users.routes());
 
-app.use(co.wrap(function* () {
-  return yield users.allowedMethods(); 
-}));
+app.use(users.allowedMethods());
 
-app.use(devMiddleware(compile, {
-    // display no info to console (only warnings and errors)
-    noInfo: false,
-
-    // display nothing to the console
-    quiet: false,
-
-    // switch into lazy mode
-    // that means no watching, but recompilation on every request
-    lazy: true,
-
-    // watch options (only lazy: false)
+app.use(middleware({
+  dev: {
+    output: webpackConfig.output,
+    entry: webpackConfig.entry,
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    },
     watchOptions: {
         aggregateTimeout: 300,
         poll: true
-    },
-
-    // public path to bind the middleware to
-    // use the same as in webpack
-    publicPath: "/src/",
-
-    // custom headers
-    headers: { "X-Custom-Header": "yes" },
-
-    // options for formating the statistics
-    stats: {
-        colors: true
     }
+  }
 }));
-
-app.use(hotMiddleware(compile, {
-  // log: console.log,
-  // path: '/__webpack_hmr',
-  // heartbeat: 10 * 1000
-}));
-
 
 https.createServer(
     options, 
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,56 @@
 const webpack = require('webpack');
+const path = require('path');  
+
+const ROOT_PATH = path.resolve(__dirname);
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     App: './src/js/components/App.jsx'
   },
   output: {
-    path: __dirname + '/build',
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: '[name].js',
+    publicPath: path.resolve(ROOT_PATH, 'build')
+  },
+  devServer: {
+    contentBase: path.resolve(ROOT_PATH, 'build'),
+    publicPath: path.resolve(ROOT_PATH, 'build'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    port: 3000,
     filename: '[name].js'
   },
-  devtool: 'source-map',
+  devtool: '#eval-source-map',
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loader: 'babel',
+      loader: 'babel-loader',
+      exclude: /node_modules/,
       query: {
-        presets:['es2015', 'react', 'stage-0', 'react-hmre']
-      },
-      include: './src'
+        presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
+        plugins: ["transform-runtime", "transform-react-jsx"],
+      }
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   externals: {
     react: 'React'
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new (webpack.optimize.OccurenceOrderPlugin || webpack.optimize.OccurrenceOrderPlugin)(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: path.resolve(ROOT_PATH, 'views/index.html'),
+      template: path.resolve(ROOT_PATH, 'views/index.html'),
+      inject: true
+    })
   ]
 };
